68ba9fa15400899a50e97e3d8f4d7027c00a473b
Michael Schumacher <mschuamcher@carlson.com>
2019-02-22 15:23:46 -0600

    Save it

---
+++ ch03/tacos-jpa/src/main/java/tacos/security/SecurityConfig.java
@@ -0,0 +1,15 @@
+package tacos.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
